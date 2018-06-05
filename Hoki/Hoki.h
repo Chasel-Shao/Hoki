e73a5569f852ad1c1b2088b4ec687101b88a13de
shaowentao <shaowentao@bilibili.com>
2018-06-05 20:28:56 +0800

    Initial Commit

---
+++ Hoki/Hoki.h
@@ -0,0 +1,19 @@
+//
+//  Hoki.h
+//  Hoki
+//
+//  Created by Chasel on 2018/6/5.
+//  Copyright © 2018 邵文涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hoki.
+FOUNDATION_EXPORT double HokiVersionNumber;
+
+//! Project version string for Hoki.
+FOUNDATION_EXPORT const unsigned char HokiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hoki/PublicHeader.h>
+
+
